023711108d3c34a1ed3d4ed2bcffd886f6ae356a
harshit <harshitkumar52340@gmail.com>
2026-06-30 15:48:16 +0530

    Generate similarity matrix in app

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 def fetch_poster(movie_id):
     try:
@@ -20,34 +23,41 @@ def fetch_poster(movie_id):
     except requests.exceptions.RequestException:
         return "https://via.placeholder.com/500x750?text=No+Poster"
 
+
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies=[]
-    recommended_movies_posters=[]
-    for i in movies_list:
-        movie_id=movies.iloc[i[0]].movie_id
+    recommended_movies = []
+    recommended_movies_posters = []
 
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_posters.append(fetch_poster(movie_id))
+
     return recommended_movies, recommended_movies_posters
 
-movies=pickle.load(open('movies.pkl', 'rb'))
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-movies_list=movies['title'].values
+movies = pickle.load(open('movies.pkl', 'rb'))
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+movies_list = movies['title'].values
 
 st.title('movies recommender system')
 
 selected_movie_name = st.selectbox(
-    "'Select your movie from drop down menu or search for a movie'",
-    movies_list)
+    "Select your movie from drop down menu or search for a movie",
+    movies_list
+)
 
 if st.button("Recommend"):
     with st.spinner("Fetching recommendations..."):
-        names, posters=recommend(selected_movie_name)
+        names, posters = recommend(selected_movie_name)
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
